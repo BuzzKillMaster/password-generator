cd7bc9ee325e1e0271d68d11876ca1f2d180ddce
BuzzKill Master <BuzzKillMaster@proton.me>
2023-02-25 12:33:44 +0100

    move checkable values type declaration to its own file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import React, {useState} from "react";
 import {FaRegCopy} from "react-icons/fa";
 import CheckBox from "@/components/CheckBox";
-
-type CheckableValues = {
-    uppercase: boolean
-    lowercase: boolean
-    numbers: boolean
-    symbols: boolean
-}
+import CheckableValues from "@/types/CheckableValues";
 
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
---
+++ src/types/CheckableValues.ts
@@ -0,0 +1,8 @@
+type CheckableValues = {
+    uppercase: boolean
+    lowercase: boolean
+    numbers: boolean
+    symbols: boolean
+}
+
+export default CheckableValues
